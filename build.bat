6184a71c6871aece855dc56fa04ae8175ffa207b
Rory Bradford <roryrjb@gmail.com>
2026-02-21 15:15:54 +0000

    Fix build

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
-pip install -r requirements.txt
-uvx pyinstaller --onefile --name media-tool media-tool.py
+uv venv
+call .venv\Scripts\activate
+uv pip install -r requirements.txt
+uv run pyinstaller --onefile --name media-tool media-tool.py
